a70278c80e89570c9b11c4560edff135395e4a72
SBDavid <billl89@126.com>
2017-10-30 23:59:41 +0800

    图片懒加载

--- front/src/list.js
+++ front/src/list.js
@@ -1,6 +1,6 @@
 require('jquery-lazyload');
-var config = require('../../httpServer/apps/config');
+
 $("img[data-original]").lazyload({
     failurelimit : 10,
-    placeholder: config.getUrl('static') + '/list/static/css/lazyload.svg'
+    placeholder: STATIC_URL + '/list/static/css/lazyload.svg'
 });
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,12 +1,19 @@
 var path = require('path');
+var webpack = require("webpack");
+var config = require('../httpServer/apps/config'); 
 
 module.exports = {
   entry: {
-    'article':'./src/article.js', 
+    'article': './src/article.js',
     'list': './src/list.js'
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '../httpServer/static/js')
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      STATIC_URL: JSON.stringify(config.getUrl('static'))
+    })
+  ]
 };
