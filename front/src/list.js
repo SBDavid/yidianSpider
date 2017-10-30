b9d89ae517b5ba5cae000d25a2b22066301d13f2
SBDavid <billl89@126.com>
2017-10-30 23:43:03 +0800

    图片懒加载

--- front/src/list.js
+++ front/src/list.js
@@ -1,5 +1,6 @@
 require('jquery-lazyload');
+var config = require('../../httpServer/apps/config');
 $("img[data-original]").lazyload({
     failurelimit : 10,
-    src: 'data-original'
+    placeholder: config.getUrl('static') + '/list/static/css/lazyload.svg'
 });
