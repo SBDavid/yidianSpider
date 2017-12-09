5ea6d80ff3da9082441a0887f8db55ab14ef8e38
davidtangjw <davidtangjw@pptv.com>
2017-12-09 18:51:24 +0800

    baidupush

--- fetchAtical/persistence/schema/baidupush.js
+++ fetchAtical/persistence/schema/baidupush.js
@@ -3,7 +3,7 @@ var mongoose = require('../connection');
 var baidupushSchema = mongoose.Schema({
     url: { type: String },
     action: { type: String },
-    isDone: { type: String, default: false }
+    isDone: { type: boolean, default: false }
 }, { 
     autoIndex: false,
     _id: true,
